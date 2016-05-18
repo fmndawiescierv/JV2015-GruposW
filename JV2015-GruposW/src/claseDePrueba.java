52bac3f6b95640d7791f9780cb301e242e790349
Kubuntu-Laptop <Kubuntu-Laptop@LAPTOP>
2016-05-18 20:02:21 +0100

    Prueba de commit

---
+++ JV2015-GruposW/src/claseDePrueba.java
@@ -0,0 +1,11 @@
+// Prueba
+public class claseDePrueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("prueba commit Fulgencio Montoya");
+
+	}
+
+}
